2d71be24928a56f1f515622c5907754ba99a134c
lsaccoz <luigisacco7@gmail.com>
2016-09-15 17:05:28 -0700

    Update updatebalance.v

--- lab1/Task3.4/updatebalance.v
+++ lab1/Task3.4/updatebalance.v
@@ -2,16 +2,20 @@ module updatebalance(dealerwin, playerwin, currentbettype, currentbetamount, cur
 	input dealerwin, playerwin;
 	input [1:0] currentbettype;
 	input [7:0] currentbalance;
-	output [7:0] updatebalance;
+	output reg [7:0] updatebalance;
+	reg ch;
 	
 	always @(*) begin
-		if(dealerwin && playerwin && (currentbettype==2'b11||currentbettype=2'b00)
-			updatebalance = currentbalance + currentbetamount;
-		else if(dealerwin && (currentbettype==2'b10))
-			updatebalance = currentbalance + currentbetamount;
-		else if(playerwin && (currentbettype==2'b01))
-			updatebalance = currentbalance + currentbetamount;
-		else
+		ch = 1'b0;
+		if (currentbalance + currentbetamount < 256) begin
+			if (dealerwin && playerwin && (currentbettype==2'b11||currentbettype=2'b00))
+				{updatebalance, ch} = {currentbalance + currentbetamount, 1'b1};
+			else if (dealerwin && (currentbettype==2'b10))
+				{updatebalance, ch} = {currentbalance + currentbetamount, 1'b1};
+			else if (playerwin && (currentbettype==2'b01))
+				{updatebalance, ch} = {currentbalance + currentbetamount, 1'b1};
+		end
+		else if (currentbalance - currentbetamount >= 0 && ~ch)
 			updatebalance = currentbalance - currentbetamount;
 	end
 endmodule
