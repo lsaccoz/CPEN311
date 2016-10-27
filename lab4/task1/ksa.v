1b2153fe90e6aaf89d4e302c0499a8e715022965
peterdeutsch <peterdeutsch42@gmail.com>
2016-10-27 15:21:04 -0700

    Working stage 1

--- lab4/task1/ksa.v
+++ lab4/task1/ksa.v
@@ -18,7 +18,7 @@ reg wren;
 
 // include S memory structurally
 
-oneport u0(address, clk, data, wren, q);
+s_memory u0(address, CLOCK_50, data, wren, q);
 
 // Write your code here.  As described in the lectures, this code will drive
 // the address, data, and wren signals to fill the memory with the values 0..255.
@@ -26,9 +26,36 @@ oneport u0(address, clk, data, wren, q);
 // You will likely be writing a state machine.  Ensure that after the memory is
 // filled, you enter a DONE state which does nothing but loop back to itself.
 
+reg [7:0]i;
 
-
+always @(posedge CLOCK_50) begin
+	case (state)
+	
+		state_init: begin
+			i=0;
+			address = i;
+			data = i;
+			wren = 1'b1;
+			state <= state_fill;
+		end
+		
+		state_fill: begin
+			i = i+1;
+			address = i;
+			data = i;
+			wren = 1'b1;
+			if(i>256)
+				state <= state_done;
+			else
+				state <= state_fill;
+		end
+		
+		state_done: begin
+			wren = 0;
+			state<=state_done;
+		end
+				
+		default: state<=state_init;
+	endcase
+end
 endmodule
-
-
-
